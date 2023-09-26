cfd2bcb1dd9934373a699bcf3dc8486688cb1364
Mohammad Hossein <mh.mehryar@gmail.com>
2023-09-26 22:59:00 +0330

    CMP-3 AddConfiguration-PackagesInstalled

---
+++ ClientManagementApi.Infrastrucure.Persistence/Configuration/ClientConfig.cs
@@ -0,0 +1,15 @@
+﻿using ClientManagementApi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientManagementApi.Infrastrucure.Persistence.Configuration
+{
+    public class ClientConfig:IEntityTypeConfiguration<Client>
+    {
+
+    }
+}
