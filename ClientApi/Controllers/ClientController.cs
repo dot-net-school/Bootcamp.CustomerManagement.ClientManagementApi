be7f23944745404d38fd3cdd80d6355f0cd5be42
Mohammad Hossein <mh.mehryar@gmail.com>
2023-10-10 20:22:22 +0330

    CMP-18 Comes to CMP-10 GetClientForScoring

--- ClientApi/Controllers/ClientController.cs
+++ ClientApi/Controllers/ClientController.cs
@@ -1,4 +1,5 @@
-using ClientManagementApi.Application.Clients.Command;
+using ClientManagementApi.Application.Clients.Command.CreateClient;
+using ClientManagementApi.Application.Clients.Queries.GetClinetForScoring;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ClientApi.Controllers;
@@ -7,7 +8,17 @@ namespace ClientApi.Controllers;
 [Route("[controller]")]
 public class ClientController : BaseController
 {
+
+    [HttpGet]
+
+    public async Task<ActionResult<GetClinetForScoringDto>> GetClient([FromQuery] GetClientForScoringQuery query)
+    => await Mediator.Send(query);
+
+    
+
     [HttpPost]
     public async Task<ActionResult<Guid>> Create(CreateClientCommand command)
            => await Mediator.Send(command);
+
+
 }
