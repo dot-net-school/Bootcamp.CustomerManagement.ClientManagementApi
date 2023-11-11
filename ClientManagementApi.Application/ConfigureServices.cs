150a15e5d5c77fd5eadc22271aa6e9f8919d3a64
Nazanin Nobakht <125179313+nazaninnobakht@users.noreply.github.com>
2023-11-11 23:10:35 +0330

    AppendValidation - CMP19

--- ClientManagementApi.Application/ConfigureServices.cs
+++ ClientManagementApi.Application/ConfigureServices.cs
@@ -1,6 +1,8 @@
 ﻿
 using ClientManagementApi.Application.Common.Interfaces;
 using FluentValidation;
+using FluentValidation.AspNetCore;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System.Reflection;
 
@@ -12,13 +14,15 @@ public static class ConfigureServices
 {
     public static IServiceCollection RegisterApplicationServices(this IServiceCollection services)
     {
-       // services.AddControllers().AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<AnotherValidationClass>());
+        //services.AddControllers().AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<AnotherValidationClass>());
 
         services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        services.AddFluentValidationAutoValidation().AddFluentValidationClientsideAdapters();
 
         services.AddMediatR(cfg =>
         {
             cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            //cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour<,>));
         }
         );
 
